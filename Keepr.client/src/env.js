d66d3bbdbf5095db03e496e6897929063e3cb62a
KranberryKin <quinnclimbs@gmail.com>
2021-10-29 12:50:24 -0600

    interfaces added

--- Keepr.client/src/env.js
+++ Keepr.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-9d7awe35.us.auth0.com'
+export const clientId = 'xlZqY7y0mzN8kAti5Kcqh1RSZiwtWPS6'
+export const audience = 'https://gregslist-auth.com'
---
+++ Keepr/Interfaces/IService.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace Keepr.Interfaces
+{
+  public interface IService<T>
+  {
+        /// <summary>
+    /// Gets all items of a table, returns a list.
+    /// </summary>
+    List<T> Get();
+/// <summary>
+/// Returns a single items, find it by it's Id.
+/// </summary>
+    T Get(int id);
+    /// <summary>
+    /// Adds the new item to the database and returns it.
+    /// </summary>
+    /// <returns>Created Object</returns>
+    T Create(int id);
+    /// <summary>
+    /// Takes updated 
+    /// </summary>
+    /// <returns>Edited Object</returns>
+    T Edit(int id, T data);
+    /// <summary>
+    /// Finds item by it's Id and removes it form the database.
+    /// </summary>
+    void Delete(int id);
+  }
+}
