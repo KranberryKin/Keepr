f1cded8a2d98d8350e916a53f88925d43e6df140
KranberryKin <quinnclimbs@gmail.com>
2021-10-29 10:55:49 -0600

    Server Models Done

--- Keepr/Models/Account.cs
+++ Keepr/Models/Account.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-
 namespace Keepr.Models
 {
-    public class Account
+  public class Account : Profile
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
         public string Email { get; set; }
-        public string Picture { get; set; }
     }
 }
---
+++ Keepr/Models/Profile.cs
@@ -0,0 +1,10 @@
+namespace Keepr.Models
+{
+  public class Profile
+  {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+
+  }
+}
