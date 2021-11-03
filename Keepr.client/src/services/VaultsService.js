d2560f4ec669fabd74108bcfac595dbd6275e367
KranberryKin <quinnclimbs@gmail.com>
2021-11-03 15:00:06 -0600

    vaults service done

--- Keepr.client/src/services/VaultsService.js
+++ Keepr.client/src/services/VaultsService.js
@@ -2,6 +2,7 @@ import { AppState } from "../AppState"
 import { VaultModel } from "../models/VaultModel"
 import { logger } from "../utils/Logger"
 import { api } from "./AxiosService"
+import { keepsService } from "./KeepsService"
 
 class VaultsService{
 async GetVaults(){
@@ -14,6 +15,23 @@ async GetVaultById(vaultId){
   const res = await api.get('api/vaults/' + vaultId)
   logger.log("get vault by id res", res)
   AppState.activeVault = new VaultModel(res.data)
+}
+async CreateVault(vaultData){
+  const res = await api.post('api/vaults', vaultData)
+  logger.log("Create vault res", res)
+  AppState.vaults = [new VaultModel(res.data), ...AppState.vaults]
+}
+async EditVault(vaultData){
+  const res = await api.put('api/vaults/' + vaultData.id, vaultData)
+  logger.log("Edit vault res", res)
+  let vaultIndex = AppState.vaults.findIndex(v => v.id === vaultData.id)
+  AppState.vaults.splice(vaultIndex, 1, new VaultModel(res.data))
+}
+async DeleteVault(vaultId){
+  const res = await api.delete('api/vaults/' + vaultId)
+  logger.log("Delete vault res", res)
+  let vaultIndex = AppState.vaults.findIndex(v => v.id === vaultId)
+  AppState.vaults.splice(vaultIndex, 1)
 }
 }
 
